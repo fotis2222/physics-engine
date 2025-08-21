a2c59815a5d96ffe42a3bad8c29b6eb094efdf99
fotis2222 <nitsas2012@gmail.com>
2025-08-21 18:50:09 +0300

    added some cool new things
    e - explode
    vine boom
    ball limit of 200

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using System.Reflection.Metadata;
 using Raylib_cs;
 
 class Program
@@ -7,11 +8,13 @@ class Program
     static Random random = new();
 
     static int r, g, b;
+    static Sound vineBoom = Raylib.LoadSound("assets/sounds/VINE BOOM.wav");
 
     static void Main()
     {
         Raylib.InitWindow(800, 600, "physics");
         Raylib.SetTargetFPS(60);
+        Raylib.InitAudioDevice();
 
         while (!Raylib.WindowShouldClose())
         {
@@ -26,13 +29,27 @@ class Program
     static void Draw()
     {        
         Raylib.ClearBackground(Color.Black);
-        if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+        if (Raylib.IsMouseButtonPressed(MouseButton.Left) || (Raylib.IsMouseButtonDown(MouseButton.Left) && Raylib.IsKeyDown(KeyboardKey.LeftShift)))
         {
             r = random.Next(30, 256);
             g = random.Next(30, 256);
             b = random.Next(30, 256);
             Vector2 mousePos = Raylib.GetMousePosition();
             balls.Add(new Ball(mousePos.X, mousePos.Y, 20, new Color(r, g, b), -200, (float)(random.NextDouble() * 10 - 5)));
+            if (balls.Count > 200)
+            {
+                balls.RemoveAt(0);
+            }
+        }
+
+        if (Raylib.IsKeyPressed(KeyboardKey.E))
+        {
+            Raylib.PlaySound(vineBoom);
+            foreach (var ball in balls)
+            {
+                ball.xVelocity = random.Next(-10, 10);
+                ball.yVelocity = random.Next(-2000, 2000);
+            }
         }
 
         foreach (var ball in balls)
